6ead42854f03f8668e97b2a0910283c0bea17d5d
freillat <96790464+freillat@users.noreply.github.com>
2025-06-10 17:35:44 +0000

    q3

---
+++ 01-Introduction/HW1.py
@@ -0,0 +1,70 @@
+import requests
+from elasticsearch import Elasticsearch
+from tqdm.auto import tqdm
+
+docs_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main/01-intro/documents.json?raw=1'
+docs_response = requests.get(docs_url)
+documents_raw = docs_response.json()
+
+documents = []
+
+for course in documents_raw:
+    course_name = course['course']
+
+    for doc in course['documents']:
+        doc['course'] = course_name
+        documents.append(doc)
+
+print(f"Loaded {len(documents)} documents.")
+print(documents[0])
+
+es_client = Elasticsearch("http://localhost:9200")
+print(es_client.info())
+
+index_settings = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "properties": {
+            "text": {"type": "text"},
+            "section": {"type": "text"},
+            "question": {"type": "text"},
+            "course": {"type": "keyword"} 
+        }
+    }
+}
+
+index_name = "course-questions"
+
+# es_client.indices.create(index=index_name, body=index_settings)
+# for doc in documents:
+#     es_client.index(index=index_name, document=doc)
+
+query = "How do execute a command on a Kubernetes pod?"
+
+search_query = {
+    "size": 5,
+    "query": {
+        "bool": {
+            "must": {
+                "multi_match": {
+                    "query": query,
+                    "fields": ["question^4", "text"],
+                    "type": "best_fields"
+                }
+            # },
+            # "filter": {
+            #     "term": {
+            #         "course": "data-engineering-zoomcamp"
+            #     }
+            }
+        }
+    }
+}
+
+response = es_client.search(index=index_name, body=search_query)
+top_score = response['hits']['hits'][0]['_score']
+
+print(top_score)
