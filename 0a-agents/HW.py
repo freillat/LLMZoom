c6522232a3e6cef9e041591783659586d56b03b7
freillat <96790464+freillat@users.noreply.github.com>
2025-07-14 15:20:47 +0000

    start

---
+++ 0a-agents/HW.py
@@ -0,0 +1,13 @@
+import random
+
+known_weather_data = {
+    'berlin': 20.0
+}
+
+def get_weather(city: str) -> float:
+    city = city.strip().lower()
+
+    if city in known_weather_data:
+        return known_weather_data[city]
+
+    return round(random.uniform(-5, 35), 1)
